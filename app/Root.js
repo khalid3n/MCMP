21d3a523944f2595a88520b7af4100876ece25c3
Khalid3n <khalid3n@yahoo.com>
2017-12-04 22:45:23 -0800

    replaced login jsx with container in root

--- app/Root.js
+++ app/Root.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import DevTools from './DevTools';
 import configureStore from './state/configureStore';
-import Login from './login/Login.view';
+import Login from './login/Login.container';
 
 const isDev = process.env.NODE_ENV === 'development';
 const store = configureStore();
